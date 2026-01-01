df480bbfd8914e1fd69bc76ca6734a6c55e8b630
David Hawley Brim <davidbrim@outlook.com>
2026-01-01 13:28:11 -0600

    Delete migrations

--- migrations/0002_bent_rage.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE "accounts_table" ALTER COLUMN "id" SET DATA TYPE varchar(5);--> statement-breakpoint
-ALTER TABLE "accounts_table" ALTER COLUMN "route_id" SET DATA TYPE varchar(5);--> statement-breakpoint
-ALTER TABLE "routes_table" ALTER COLUMN "id" SET DATA TYPE varchar(5);--> statement-breakpoint
-ALTER TABLE "routes_table" ALTER COLUMN "user_id" SET DATA TYPE varchar(5);--> statement-breakpoint
-ALTER TABLE "users_table" ALTER COLUMN "id" SET DATA TYPE varchar(5);
--- src/schema.ts
+++ src/schema.ts
@@ -46,19 +46,6 @@ export const accountsTable = pgTable('accounts_table', {
     zipCode: text('zip_code'),
 });
 
-// export const postsTable = pgTable('posts_table', {
-//   id: serial('id').primaryKey(),
-//   title: text('title').notNull(),
-//   content: text('content').notNull(),
-//   userId: integer('user_id')
-//     .notNull()
-//     .references(() => usersTable.id, { onDelete: 'cascade' }),
-//   createdAt: timestamp('created_at').notNull().defaultNow(),
-//   updatedAt: timestamp('updated_at')
-//     .notNull()
-//     .$onUpdate(() => new Date()),
-// });
-
 export type InsertUser = typeof usersTable.$inferInsert;
 export type SelectUser = typeof usersTable.$inferSelect;
 
